dfa16a3213fb5776f390dfbde67369bd96aee12b
Ryordan Panter <ryordan23@gmail.com>
2020-07-02 00:15:07 +1000

    part one

---
+++ 100DaysOfSwift/Project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Ryordan Panter on 1/7/20.
+//  Copyright © 2020 Ryordan Panter. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
