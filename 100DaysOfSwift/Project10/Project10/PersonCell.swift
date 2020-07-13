8589a74b891f419c91457e4d79dcc91d2fb0676e
Ryordan Panter <ryordan23@gmail.com>
2020-07-13 23:33:45 +1000

    init project10 set up collectionView

---
+++ 100DaysOfSwift/Project10/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Ryordan Panter on 13/7/20.
+//  Copyright © 2020 Ryordan Panter. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ 100DaysOfSwift/Project10/Project10/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Ryordan Panter on 13/7/20.
+//  Copyright © 2020 Ryordan Panter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        return cell
+    }
+
+
+}
+
