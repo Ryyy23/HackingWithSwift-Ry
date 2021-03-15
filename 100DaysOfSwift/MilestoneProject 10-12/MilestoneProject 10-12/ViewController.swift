878affdade75491e111354dd0780b9d5b384d405
Ryordan Panter <ryordan23@gmail.com>
2021-03-15 15:57:27 +1100

    init milestone project 10-12

---
+++ 100DaysOfSwift/MilestoneProject 10-12/MilestoneProject 10-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MilestoneProject 10-12
+//
+//  Created by Ryordan Panter on 15/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
