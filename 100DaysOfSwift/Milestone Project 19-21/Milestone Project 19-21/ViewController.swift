c1a3076ffd41023d76ecf9922470783547609c5d
Ryordan Panter <ryordan23@gmail.com>
2022-02-03 16:55:10 +1100

    milestone project 19-21 init

---
+++ 100DaysOfSwift/Milestone Project 19-21/Milestone Project 19-21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone Project 19-21
+//
+//  Created by Ryordan Panter on 3/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
