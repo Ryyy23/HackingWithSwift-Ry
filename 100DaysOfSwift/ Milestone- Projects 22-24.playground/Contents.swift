4daab3b1283d8595ede0bc2e944fdea4966954f3
Ryordan Panter <ryordan23@gmail.com>
2024-03-26 20:54:09 +1100

    Milestone: Projects 22-24 complete

---
+++ 100DaysOfSwift/ Milestone- Projects 22-24.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+extension UIView {
+    func bounceOut(duration: TimeInterval) {
+        UIView.animate(withDuration: duration) { [unowned self] in
+            self.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+        }
+    }
+}
+extension Int {
+    func times(_ closure: () -> Void){
+        guard self > 0 else {return}
+        
+        for _ in 0 ..< self {
+            closure()
+        }
+    }
+}
+
+let num: Int = 5
+num.times {
+    print("Hello!")
+}
+
+
+extension Array where Element: Comparable {
+    mutating func remove(item: Element) {
+        if let location = self.firstIndex(of: item){
+            self.remove(at: location)
+        }
+    }
+}
+let view = UIView()
+view.bounceOut(duration: 3)
+
+5.times { print("Hello") }
+
+var numbers = [1, 2, 3, 4, 5]
+numbers.remove(item: 3)
